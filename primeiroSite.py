38f54df99a12046de80363b53c43a5895a90e2f4
Tabata9 <100172853+Tabata9@users.noreply.github.com>
2023-04-13 19:37:15 -0300

    Add files via upload

---
+++ primeiroSite.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+#route -> saolourencodaserradoacoes.com/contatos
+#função -> o que você quer exibir naquela página
+
+@app.route("/")
+def homepage():
+    return "Vamos pra cima"
+
+@app.route("/contatos")
+def contatos():
+    return "Nossos contatos são: E-mail: trabalhounivesp@gmail.com Telefone(11)99999-9999"
+
+@app.route("/crianca")
+def infantil():
+    return "AQUI AS CRIANÇAS BRINCAM E SE DIVERTEM"
+#colocar o site no ar
+
+if __name__=="__main__":
+    app.run(debug=True)
