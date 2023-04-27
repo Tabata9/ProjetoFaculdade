cf7842e1196a859f9c7a89396d8a9ccd7e68404f
Tabata9 <tabatarsouza@gmail.com>
2023-04-27 18:11:50 -0300

    envio padrão

--- primeiroSite.py
+++ primeiroSite.py
@@ -6,11 +6,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return "Vamos pra cima"
+    return "Aqueça seu coração"
 
 @app.route("/contatos")
 def contatos():
     return "Nossos contatos são: E-mail: trabalhounivesp@gmail.com Telefone(11)99999-9999"
+
+@app.route("/quem_somos")
+def informações():
+    return "Somos um canal de doação focado em conectar doadores a receptores e assim facilitar o modo como as doações são destinadas, fazendo a ponte para que as pessoas recebam o que de fato precisam.   "
+
+@app.route("/quero_receber")
+def receptores():
+    return "Aqui você pode escolher quais são suas necessidades e aguardar um doador"
+
+@app.route("/quero_doar")
+def doares():
+    return "Aqui você apoia um receptor e faz sua doação"
+
+
 #colocar o site no ar
 
 if __name__=="__main__":
